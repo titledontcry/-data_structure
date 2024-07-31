38a4674f33de0aa20661187b181a293aa22baae5
Josu_Title <138316042+titledontcry@users.noreply.github.com>
2024-08-01 02:15:22 +0700

    Add files via upload

---
+++ lab4_2/lab4_2_2.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+typedef struct node {
+    int data;
+    struct node *next;
+} node;
+
+node *head = NULL;
+
+node* newNode(int v) {
+    node *new_node = (node*)malloc(sizeof(node));
+    new_node->data = v;
+    new_node->next = NULL;
+    return new_node;
+}
+void DisplayLL() {
+    node *ptr = head;
+    while(ptr != NULL) {
+        printf("%d -> ", ptr->data);
+        ptr = ptr->next;
+    }
+    printf("NULL\n");
+}
+bool isCircular(node *h) {
+    if (h == NULL) {
+        return 0;
+    }
+    node *move_1 = head;
+    node *move_2 = head;
+    while (move_2 != NULL && move_2->next != NULL) {
+        move_1 = move_1->next;
+        move_2 = move_2->next->next;
+        if (move_1 == move_2) {
+            return true;
+        }
+    }
+    return false;
+}
+int main() {
+    head = newNode(1);
+    head->next = newNode(2);
+    head->next->next = newNode(3);
+    head->next->next->next = newNode(4);
+
+    DisplayLL();
+    if (isCircular(head)) {
+        printf("this LL is circular\n");
+    } else {
+        printf("this LL isn't circular\n");
+    }
+    return 0;
+}
---
+++ lab4_2/lab4_2_4.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int data;
+    struct node *next;
+    struct node *prev;
+} node;
+
+node *head = NULL;
+
+node* newNode(int v) {
+    node *new_node = (node*)malloc(sizeof(node));
+    new_node->data = v;
+    new_node->next = NULL;
+    new_node->prev = NULL;
+    return new_node;
+}
+ v v
+void DisplayLL() {
+    node *ptr = head;
+    while (ptr != NULL) {
+        printf("%d -> ", ptr->data);
+        ptr = ptr->next;
+    }
+    printf("NULL\n");
+}
+
+void doublyMaker() {
+    if (head == NULL) return;
+    node *curr = head;
+    node *prev = NULL;
+    while (curr != NULL) {
+        curr->prev = prev;
+        prev = curr;
+        curr = curr->next;
+    }
+}
+void reverseDisplay() {
+    if (head == NULL) return;
+
+    node *ptr = head;
+    while (ptr->next != NULL) {
+        ptr = ptr->next;
+    }
+    while (ptr != NULL) {
+        printf("%d ", ptr->data);
+        ptr = ptr->prev;
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    for (int i = n; i > 0; i--) {
+        node *new_node = newNode(i);
+        new_node->next = head;
+        if (head != NULL) {
+            head->prev = new_node;
+        }
+        head = new_node;
+    }
+
+    doublyMaker();
+    reverseDisplay();
+
+    return 0;
+}
